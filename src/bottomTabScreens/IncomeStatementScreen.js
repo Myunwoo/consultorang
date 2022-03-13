34f212b527127253a50b92daa1a0e6b4ca805bb0
Myunwoo <63415602+Myunwoo@users.noreply.github.com>
2022-03-13 20:29:47 +0900

    a

--- src/bottomTabScreens/IncomeStatementScreen.js
+++ src/bottomTabScreens/IncomeStatementScreen.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, Pressable } from 'react-native';
-<<<<<<< HEAD
 import {LinearGradient} from 'expo-linear-gradient';
 
 import { theme } from '../variables/color';
@@ -68,24 +67,6 @@ const IncomeStatementScreen = (({navigation}) => {
                 </View>
             </View>
         </LinearGradient>
-=======
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-
-const IncomeStatementScreen = (({navigation}) => {
-    const temp=()=>{
-        navigation.replace('Auth');
-        AsyncStorage.setItem('autoLogin', 'false');
-        AsyncStorage.setItem('emailSave', 'false');
-    }
-
-    return (
-        <View style={{width:'100%', height:'100%', justifyContent:'center', alignItems:'center',}}>
-            <Pressable onPress={temp}>
-                <Text>로그아웃</Text>
-            </Pressable>
-        </View>
->>>>>>> master
     );
 });
 
